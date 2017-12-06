70b4caedc47652cdf76b4d4bd60f45358565b15b
Torin Nguyen <torinnguyen@gmail.com>
2017-12-06 17:17:03 +0700

    Add a few interfaces between webview and business code

--- KeyReply/Classes/KEYKeyReplyView.m
+++ KeyReply/Classes/KEYKeyReplyView.m
@@ -8,9 +8,22 @@
 #import <WebKit/WebKit.h>
 #import "KEYKeyReplyView.h"
 
-#define DEMO_URL        @"https://files.keyreply.com/demo/index.html"
+#ifdef PRODUCTION
+    //Actual production
+    #define DEMO_URL                @"https://files.keyreply.com/demo/index.html"
+#else
+#ifdef DEBUG
+    //Debug
+    #define DEMO_URL                @"https://files.keyreply.com/demo/index.html"
+#else
+    //Staging, archive for private beta
+    #define DEMO_URL                @"https://files.keyreply.com/demo/index.html"
+#endif
+#endif
 
-@interface KEYKeyReplyView() <WKNavigationDelegate, WKUIDelegate>
+#define SDK_URL_SCHEME          @"keyreplysdk://"
+
+@interface KEYKeyReplyView() <WKNavigationDelegate, WKScriptMessageHandler, WKUIDelegate>
 @property (nonatomic, strong) WKWebView * webView;
 @end
 
@@ -47,10 +60,11 @@
 {
     NSString *jScript = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); document.getElementsByTagName('head')[0].appendChild(meta);";
     WKUserScript * wkUScript = [[WKUserScript alloc] initWithSource:jScript injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
-    WKUserContentController *wkUController = [[WKUserContentController alloc] init];
-    [wkUController addUserScript:wkUScript];
+    WKUserContentController *wkUserController = [[WKUserContentController alloc] init];
+    [wkUserController addUserScript:wkUScript];
+    [wkUserController addScriptMessageHandler:self name:@"documentReady"];
     WKWebViewConfiguration *wkWebConfig = [[WKWebViewConfiguration alloc] init];
-    wkWebConfig.userContentController = wkUController;
+    wkWebConfig.userContentController = wkUserController;
     
     WKWebView * webView = [[WKWebView alloc] initWithFrame:self.bounds configuration:wkWebConfig];
     webView.backgroundColor = [UIColor clearColor];
@@ -59,6 +73,7 @@
     webView.navigationDelegate = self;
     webView.UIDelegate = self;
     webView.navigationDelegate = self;
+    webView.userCo
     webView.scrollView.showsVerticalScrollIndicator = NO;
     webView.scrollView.showsHorizontalScrollIndicator = NO;
     [self addSubview:webView];
@@ -75,6 +90,82 @@
 {
     NSURL * url = [NSURL URLWithString:urlString];
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+}
+
+
+#pragma mark - Interface to SDK
+
+- (void)evaluateJavaScript:(NSString *)javaScriptString completionHandler:(void (^ _Nullable)(_Nullable id, NSError * _Nullable error))completionHandler
+{
+    [self.webView evaluateJavaScript:javaScriptString completionHandler:^(id _Nullable results, NSError * _Nullable error) {
+        
+    }];
+}
+
+
+#pragma mark - Callback from SDK
+
+- (void)handleSDKcallback:(NSURL *)url
+{
+    //Convert the query parameters in the URL to an NSDictionary
+    NSString * query = [url query];
+    NSArray * components = [query componentsSeparatedByString:@"&"];
+    NSMutableDictionary * parameters = [[NSMutableDictionary alloc] init];
+    for (NSString * component in components) {
+        NSArray * subcomponents = [component componentsSeparatedByString:@"="];
+        if ([subcomponents count] >= 2)
+            [parameters setObject:[[subcomponents objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]
+                           forKey:[[subcomponents objectAtIndex:0] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    //Alert command
+    NSString * alertString = [parameters objectForKey:@"alert"];
+    if ([alertString length] > 0) {
+        NSString * titleString = [parameters objectForKey:@"title"];
+        UIAlertController * alertController = [UIAlertController alertControllerWithTitle:titleString
+            message:alertString
+            preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        
+        [[[UIApplication sharedApplication] keyWindow].rootViewController presentViewController:alertController animated:YES completion:nil];
+        return;
+    }
+    
+    //TODO: do something with the parameters
+}
+
+
+#pragma mark - WKNavigationDelegate
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    //Handle special keyreplysdk:// url
+    NSString * absUrl = navigationAction.request.URL.absoluteString;
+    BOOL sdkCallback = [absUrl hasPrefix:SDK_URL_SCHEME];
+    if (sdkCallback) {
+        [self handleSDKcallback:navigationAction.request.URL];
+        return;
+    }
+    
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error
+{
+    NSLog(@"%@", error);
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation
+{
+    
+}
+
+
+#pragma mark - WKScriptMessageHandler
+
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
+{
+    NSLog(@"didReceiveScriptMessage: %@", message);
 }
 
 
