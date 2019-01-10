1c06219794be755a30ace1cd83980561c141c28d
Jeremy Pek <jeremy.pek@keyreply.com>
2019-01-10 15:20:01 +0800

    new functions

--- Example/KeyReply/KEYViewController.m
+++ Example/KeyReply/KEYViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "KEYViewController.h"
-#import "KeyReply/KEYKeyReplyView.h"
+#import "KEYKeyReplyView.h"
+
 
 @interface KEYViewController ()
 @property (nonatomic, weak) IBOutlet KEYKeyReplyView * tabChatView;
 @property (nonatomic, weak) IBOutlet KEYKeyReplyView * chatView;
+@property (nonatomic, assign) CGRect chatViewFrame;
 @end
 
 @implementation KEYViewController
@@ -19,17 +21,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.chatView setServerSetting:@"https://keyreply-platform-demo-bot.azurewebsites.net"];
+    [self.chatView setEnvUrl:@"http://localhost:8081"];
+//    [self.chatView setServerSetting:@"https://keyreply-platform-demo-bot.azurewebsites.net"];
+    [self.chatView setServerSetting:@"http://localhost:3000"];
     [self.chatView reload];
-    
+    [self.tabChatView setEnvUrl:@"https://rightfrom.us/temp/keyreply/"];
     [self.tabChatView setServerSetting:@"https://keyreply-platform-demo-bot.azurewebsites.net"];
     [self.tabChatView reload];
-    
     NSMutableDictionary * userDict = [[NSMutableDictionary alloc] init];
     [userDict setValue:@"bot1" forKey:@"name"];
-    [userDict setValue:@"token" forKey:@"JWT"];
+//    [userDict setValue:@"token" forKey:@"JWT"];
     [self.chatView setUserSetting:userDict];
     [[self view] bringSubviewToFront:self.chatView];
+    [self.chatView setChatWindowResizeFunc:@selector(resize:) fromObject:self];
+    self.chatViewFrame = self.chatView.frame;
 }
 
 #pragma mark - Helper
@@ -37,15 +42,22 @@
 - (void)showAlertWithTitle:(NSString *)title message:(NSString *)message
 {
     UIAlertController * alertController = [UIAlertController alertControllerWithTitle:title
-                                                                              message:message
-                                                                       preferredStyle:UIAlertControllerStyleAlert];
+        message:message
+        preferredStyle:UIAlertControllerStyleAlert];
     [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
     [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
 #pragma mark - Actions
-
+- (void)resize:(NSString *)toggle{
+    if([toggle isEqualToString:@"true"]) {
+        self.chatView.frame = self.chatViewFrame;
+    }else {
+        CGRect newFrame = CGRectMake(1240,894,125,130);
+        self.chatView.frame = newFrame;
+    }
+}
 - (IBAction)onBtnOpen:(id)sender
 {
     [self.chatView openChatWindow];
